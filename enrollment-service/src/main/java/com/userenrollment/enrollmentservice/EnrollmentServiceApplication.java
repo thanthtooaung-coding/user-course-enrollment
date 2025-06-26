e2e34ce3968b913dabfd7d059530f6777c44bdcb
thanthtooaung <thanthtoo1285@gmail.com>
2025-06-27 03:38:13 +0700

    fixed name for enrollment service

--- enrollment-service/src/main/java/com/userenrollment/enrollmentservice/EnrollmentServiceApplication.java
+++ enrollment-service/src/main/java/com/userenrollment/enrollmentservice/EnrollmentServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class EnrollmentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(EnrollmentServiceApplication.class, args);
 	}
 
 }
